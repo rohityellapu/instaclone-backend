c8a0b0c7df4104b81d6b5962f4503e229094d2aa
rohityellapu <rohithyellapu24@gmail.com>
2022-12-14 21:58:56 +0530

    debugged at source origin value in cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(dbUrl, () => console.log("Database Connected"))
 const postRoutes = require('./routes/post')
 const cors = require('cors');
 const corsOptions = {
-    origin: 'https://instaclone-by-rohityellapu.onrender.com/',
+    origin: 'https://instaclone-by-rohityellapu.onrender.com',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
